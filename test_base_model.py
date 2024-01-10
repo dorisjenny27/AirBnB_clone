acbe44e1b91902cbd5b850afd4ef875b8d9bec9e
KalminX <noriabure@gmail.com>
2024-01-10 10:38:15 +0100

    removed my test from the tests directory

---
+++ test_base_model.py
@@ -0,0 +1,9 @@
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """The class for my test case"""
+
+
+if __name__ == '__main__':
+    unittest.main()
